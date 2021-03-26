cdfe5bcc2b91cb72c3172d886127ed3045b6cb00
taaw <taaw@users.noreply.github.com>
2021-03-26 00:18:00 -0700

    pushing shit

--- view.py
+++ view.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
 
 ## home page ##
 @app.route("/")
@@ -8,4 +10,4 @@ def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='127.0.0.1', port=5000)
