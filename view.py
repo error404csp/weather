02169e679f6edebca3a256ad64774d81e1cb0d38
Sophie Lee <57341391+sophieleeajh@users.noreply.github.com>
2021-03-31 10:11:11 -0700

    added app routes for new pages

--- view.py
+++ view.py
@@ -9,5 +9,13 @@ app.config['DEBUG'] = True
 def home():
     return render_template("home.html")
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/weather')
+def recipes():
+    return render_template('weather.html')
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)
