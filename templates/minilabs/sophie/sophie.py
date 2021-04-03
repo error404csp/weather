b9e593c58cfdfb68bd9b50b79f5770cb02ce617d
Sophie Lee <57341391+sophieleeajh@users.noreply.github.com>
2021-04-03 00:30:59 -0700

    Update sophie.py

--- templates/minilabs/sophie/sophie.py
+++ templates/minilabs/sophie/sophie.py
@@ -1,12 +1,9 @@
-"""Minilab """
 import random
 
 songlist1 = ["homemade dynamite by lorde", "pedestal by fergie", "basement by 99 neighbors", "perdus by angele", "our lady of sorrows by my chemical romance", "right and right again by sohodolls", "stfu! by rina sawayama", "酒後的心聲 by jody chiang", "biryani by ashwarya", "99 red balloons by nena", "masseduction by st. vincent", "runaway by rei ami", "nainainai by atarashii gakko!", "my hair is green by youra"]
 
 class Songs:
-    """Initializer of class takes series parameter and returns Class Object"""
     def __init__(self, series):
-        """Built in validation and exception"""
         if series < 0 or series > 15:
             raise ValueError("Series must be between 0 and 15")
         self._series = series
@@ -19,7 +16,6 @@ class Songs:
         # Instant end = Instant.now();    // time capture -- end
         # this.timeElapsed = Duration.between(start, end);
 
-    """Algorithm for building sequence, this id called from __init__"""
     def song_series(self):
         limit = self._series
         f = [random.sample((songlist1), k=2)]  # starting array/list
@@ -28,13 +24,11 @@ class Songs:
             f = [f[0]]
             limit = limit - 1
 
-    """Method/Function to set data: list, dict, and dictID are instance variables of Class"""
     def set_data(self, num):
         self._list.append(num)
         self._dict[self._dictID] = self._list.copy()
         self._dictID += 1
 
-    """Getters with decorator to allow . notation access"""
     @property
     def series(self):
         return self._series
@@ -59,8 +53,6 @@ def minilab-sophie():
 
 # Tester Code
 if __name__ == "__main__":
-    '''Value for testing'''
     n = 2
-    '''Constructor of Class object'''
     songrecs = Songs(n/n)
     print(f"Here are some song recommendations = {songrecs.list}")
