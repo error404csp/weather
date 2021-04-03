820e3fcf354b035da5e1f2f5735ae02b50ae7e8c
Sophie Lee <57341391+sophieleeajh@users.noreply.github.com>
2021-04-02 22:07:33 -0700

    Create kaila.py

---
+++ templates/minilabs/kaila/kaila.py
@@ -0,0 +1,13 @@
+class introduction:
+    def classFunction(self):
+        print("Hi my name is " + self.name + ", I am currently " + self.age + ", and I like " + self.hobby1 + ", " + self.hobby2 + ", and playing " + self.hobby3)
+
+# my class
+r1 = introduction()
+r1.name = "Kaila"
+r1.age = "17"
+r1.hobby1 = "singing"
+r1.hobby2 = "drawing"
+r1.hobby3 = "games"
+
+r1.classFunction()
