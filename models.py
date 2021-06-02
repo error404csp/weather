f8afe1481c2b35f38792e4d245cf2829b30db25b
adamh <adam.holbel@gmail.com>
2021-06-01 20:39:02 -0700

    created db and signup system

---
+++ models.py
@@ -0,0 +1,8 @@
+# from view import db, DB_NAME
+# from flask_login import UserMixin
+# from os import path
+#
+# class User(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(150), unique=True)
+#     password = db.Column(db.String(150))
--- view.py
+++ view.py
@@ -1,19 +1,37 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import UserMixin
 
-class User:
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
-users = []
-users.append(User(id=1, username='Username', password='Password'))
+# class User:
+#     def __init__(self, id, username, password):
+#         self.id = id
+#         self.username = username
+#         self.password = password
+#
+# users = []
+# users.append(User(id=1, username='Username', password='Password'))
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+
+def create_database(app):
+    if not path.exists('weather/' + DB_NAME):
+        db.create_all(app=app)
 
 
 app = Flask(__name__, template_folder="templates")
 app.secret_key = 'iswearifanotheroceanidsummonsawaterbirdiwillripitslungsout'
 app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+db.init_app(app)
+
+
 
 from minilabs.adam import adam_bp
 #from User import User
@@ -22,6 +40,8 @@ from minilabs.ethansecond import ethan2_bp
 from minilabs.kaila import kaila_bp
 from minilabs.nolan import nolan_bp
 from minilabs.sophie import sophie_bp
+
+
 app.register_blueprint(adam_bp, url_prefix='/adam')
 #app.register_blueprint(User, url_prefix='/user')
 #app.register_blueprint(ethan_bp, url_prefix='/ethan') WIP
@@ -29,6 +49,10 @@ app.register_blueprint(ethan2_bp, url_prefix='/ethan2')
 app.register_blueprint(kaila_bp, url_prefix='/kaila')
 app.register_blueprint(nolan_bp, url_prefix='/nolan')
 app.register_blueprint(sophie_bp, url_prefix='/sophie')
+
+create_database(app)
+
+
 
 ## home page ##
 @app.route("/")
@@ -46,20 +70,37 @@ def login():
         session.permanent = True
         username = request.form['username']
         password = request.form['password']
-        try:
-            user = [x for x in users if x.username == username][0]
-            if user and user.password == password:
-                session['user_id'] = user.id
-                return render_template('weather.html')
-            else:
-                return render_template('login.html')
-        except:
-            return redirect(url_for('login'))
+        # try:
+        #     user = [x for x in users if x.username == username][0]
+        #     if user and user.password == password:
+        #         session['user_id'] = user.id
+        #         return render_template('weather.html')
+        #     else:
+        #         return render_template('login.html')
+        # except:
+        #     return redirect(url_for('login'))
     else:
         if 'user' in session:
             return redirect(url_for('weather'))
     return render_template('login.html', insession = False)
 
+@app.route('/signup', methods=["POST", "GET"])
+def signup():
+
+    if request.method == "POST":
+        username = request.form['username2']
+        password = request.form['password2']
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Username Already Exists!')
+            return render_template("login.html")
+        else:
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account Created!')
+    return render_template("home.html")
 
 @app.route('/weather')
 def weather():
